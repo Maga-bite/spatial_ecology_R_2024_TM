1abba17fd522ca4e70f820a3859d00ab0acc49d2
Tommaso Magarotto <Tommymaga01@gmail.com>
2024-10-16 15:46:03 +0200

    Create 03_population_distribution_TM.R

---
+++ Code/03_population_distribution_TM.R
@@ -0,0 +1,10 @@
+#as we did for spatstat we are installing new packages
+
+install.packages("sdm")
+library(sdm)
+#this is species distribution model
+#using covariants (will be epxplained in species distribution model later)
+
+
+install.packages("terra")
+library(terra)
