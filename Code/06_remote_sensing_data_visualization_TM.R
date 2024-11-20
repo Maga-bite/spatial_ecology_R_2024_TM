71042cbc099209cddb8d12a5ffa79063812bb6b6
Tommaso Magarotto <Tommymaga01@gmail.com>
2024-11-20 15:36:31 +0100

    Update 06_remote_sensing_data_visualization_TM.R

--- Code/06_remote_sensing_data_visualization_TM.R
+++ Code/06_remote_sensing_data_visualization_TM.R
@@ -1,34 +1,3 @@
-
-#code for managing and analazing remote sensing data
-
-install.packages("devtools")
-library("devtools")
-
-#it is needed the installation of Rtools from a different installation package 
-# install.packages "Rtools"
-
-install_github("ducciorocchini/imageRy") #function from devtools for the use of the package imageRy
-
-
-library(imageRy)
-im.list()
-
-library(imageRy)
-library(terra)
-# in case you have not terra
-# install.packages("terra")
-
-# list the data
-im.list()
-
-# im. means the function is realted to duccio's pack
-# to import the data stored in the package
-b2 <- im.import("sentinel.dolomites.b2.tif") #importante to put the quotes
-#all the objects reflecting the blue light (in yellow) and the reflecting of light (dark blue)
-#change the color palette
-
-#to commit changes
-
 #code for managing and analazing remote sensing data
 
 install.packages("devtools")
@@ -44,6 +13,7 @@ library(imageRy)
 im.list()
 library(terra)
 # in case you have not terra
+
 # install.packages("terra")
 im.list() 
 #this list all the data inside the package
@@ -85,3 +55,50 @@ plot(b8, col=cl)
 sentstack<-c(b2,b3,b4,b8)
 plot(sentstack, col=cl)
 sentstack
+
+dev.off()
+#ploting one layer
+plot(sentstack[[1]], col=cl)
+
+
+plot(sentstack[[4]], col=cl)
+#let's make a multiframe with different colorpalette
+#when u plot one band u see that the original palette is going from blue to yellow 
+
+#this was to try out
+clb <- colorRampPalette(c("navyblue", "yellow", "red")) (100)
+plot(b3, col=clb)
+
+#this is an exercise
+# here is realted to the color from each band
+par(mfrow=c(2,2))
+
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+plot(b2, col=clb)
+
+clg <- colorRampPalette(c("dark green", "green", "light green")) (100)
+plot(b3, col=clg)
+
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(b4, col=clr)
+
+cln <- colorRampPalette(c("brown", "orange", "yellow")) (100)
+plot(b8, col=cln)
+
+#always using infrared gives out a lot of informtion
+#combining them in a scheme RGB in a natural color image
+#RGB plotting
+dev.off()
+im.plotRGB(sentstack, r=3, g=2, b=1)
+#now let's do the false color
+
+# RGB space
+# sentstack: 
+# band2 blue element 1, sentstack[[1]] 
+# band3 green element 2, sentstack[[2]]
+# band4 red element 3, sentstack[[3]]
+# band8 nir element 4, sentstack[[4]]
+im.plotRGB(sentstack, r=3, g=2, b=1)
+im.plotRGB(sentstack, r=4, g=3, b=2)
+im.plotRGB(sentstack, r=3, g=4, b=2)
+im.plotRGB(sentstack, r=3, g=2, b=4)
